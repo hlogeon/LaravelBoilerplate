7332b745e731bff81aee7fd247b49af71aef3d83
Andrey Degtyaruk <hlogeon1@gmail.com>
2016-10-17 18:44:08 +0700

    Codeception and DoctrineServiceProvider added

---
+++ app/Core/Providers/DoctrineServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * TODO: Configure Doctrine ODM here.
+ */
+class DoctrineServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
